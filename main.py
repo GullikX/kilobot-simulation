7c8aa189a5d1d647ddfb66802ca6a03eb5a31bb2
Martin Gulliksson <martin@gullik.cc>
2019-11-25 09:48:13 +0100

    Circle moving across screen

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python3
 import pygame
 
+windowSize = (1280, 720)
+
 
 def main():
-    print("Hello")
+    pygame.init()
+    screen = pygame.display.set_mode(windowSize)
+    pygame.display.set_caption("")
+    screen.fill((0, 0, 0))
 
+    i = 0
+
+    while True:
+        i = (i + 1) % windowSize[0]
+        screen.fill((0, 0, 0))
+        pygame.draw.circle(screen, (255, 255, 255), (i, 100), 30)
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
