d9aef12d5e9c40693bccda8e0736d7df09b9b8db
Martin Gulliksson <martin@gullik.cc>
2019-11-25 09:53:03 +0100

    Eliminate some magic numbers

--- main.py
+++ main.py
@@ -2,20 +2,23 @@
 import pygame
 
 windowSize = (1280, 720)
+backgroundColor = (0, 0, 0)
+robotColor = (128, 128, 128)
+robotSize = 30
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode(windowSize)
     pygame.display.set_caption("")
-    screen.fill((0, 0, 0))
-
     i = 0
 
     while True:
         i = (i + 1) % windowSize[0]
-        screen.fill((0, 0, 0))
-        pygame.draw.circle(screen, (255, 255, 255), (i, 100), 30)
+        robotPosition = (i, 100)
+
+        screen.fill(backgroundColor)
+        pygame.draw.circle(screen, robotColor, robotPosition, robotSize)
         pygame.display.update()
 
 if __name__ == "__main__":
