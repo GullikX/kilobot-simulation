e1c4d721db19b91d5d2eeeb166435adf32b24a0d
Morad <Moradm@student.chalmers.se>
2019-12-06 11:48:44 +0100

    added teleportation

--- kilobot.py
+++ kilobot.py
@@ -46,7 +46,13 @@ class Kilobot:
     def timestep(self, deltaTime, kilobots):
         self._setGradient(kilobots)
         nearestRobotX, nearestRobotY = self._findClosest(deltaTime, kilobots)
-        self._move(deltaTime, nearestRobotX, nearestRobotY)
+        bitMapVal = self._getBinaryCoord()
+        if bitMapVal == 0:  #move into position
+            self._move(deltaTime, nearestRobotX, nearestRobotY)
+            bitMapVal = self._getBinaryCoord()
+            while bitMapVal == 1:      #move until we leave bitShape
+                self._move(deltaTime, nearestRobotX, nearestRobotY)
+                bitMapVal = self._getBinaryCoord()
 
     def _findClosest(self, deltaTime, kilobots):
         rmax = 100
@@ -63,45 +69,25 @@ class Kilobot:
         return nearestX, nearestY
 
 
-    def getBinaryCoord(self):
-        if self.x >  0 or self.y > 0:
-            bitMapX = int(self.x/self.bitMapScalingFactor)
-            bitMapY = int(self.y/self.bitMapScalingFactor)
-
-            bitShape = self.bitMapArray.shape
-            if bitMapX >= 0 and bitMapX < bitShape[0] and bitMapY >= 0 and bitMapY < bitShape[1]:
-                print(bitMapX, bitMapY)
-                bitMapVal = self.bitMapArray[bitMapX, bitMapY]
 
     def _move(self, deltaTime, nearestX, nearestY):
-        bitMapVal = 0
-        if self.x >  0 and self.y > 0:
-            bitMapX = int(self.x/self.bitMapScalingFactor)
-            bitMapY = int(self.y/self.bitMapScalingFactor)
-            print(self.x, bitMapX)
-            bitShape = self.bitMapArray.shape
-            if bitMapX >= 0 and bitMapX < bitShape[0] and bitMapY >= 0 and bitMapY < bitShape[1]:
-                print(bitMapX, bitMapY)
-                bitMapVal = self.bitMapArray[bitMapX, bitMapY]
+        dx = self.x - nearestX
+        dy = self.y - nearestY
+        rVector = np.array([dx, dy, 0])
+        w = np.cross(rVector, np.array([0, 0, 1]))
+        tempVector = w / np.sqrt( np.dot(w, w) ) + ( preferedDistance - np.sqrt(dx**2 + dy**2) ) / ( preferedDistance - 2 * size ) * rVector / np.sqrt(np.dot(rVector,rVector))
+        preferedDirectionVector = tempVector / np.sqrt( np.dot(tempVector, tempVector) )
 
-        if bitMapVal == 0:
-            dx = self.x - nearestX
-            dy = self.y - nearestY
-            rVector = np.array([dx, dy, 0])
-            w = np.cross(rVector, np.array([0, 0, 1]))
-            tempVector = w / np.sqrt( np.dot(w, w) ) + ( preferedDistance - np.sqrt(dx**2 + dy**2) ) / ( preferedDistance - 2 * size ) * rVector / np.sqrt(np.dot(rVector,rVector))
-            preferedDirectionVector = tempVector / np.sqrt( np.dot(tempVector, tempVector) )
+        directionVector = np.array([np.cos(self.direction), np.sin(self.direction), 0])
+        choiceVector = np.dot(preferedDirectionVector, directionVector)
 
-            directionVector = np.array([np.cos(self.direction), np.sin(self.direction), 0])
-            choiceVector = np.dot(preferedDirectionVector, directionVector)
-
-            if choiceVector < np.cos(maxAngleError):
-                w = np.cross(directionVector, np.array([0, 0, 1]))
-                tempVector = np.dot(w,preferedDirectionVector)
-                self.direction -= turnSpeed * tempVector / np.sqrt( np.dot(tempVector, tempVector) )
-            else:
-                self.x += velocity * deltaTime * np.cos(self.direction)
-                self.y += velocity * deltaTime * np.sin(self.direction)
+        if choiceVector < np.cos(maxAngleError):
+            w = np.cross(directionVector, np.array([0, 0, 1]))
+            tempVector = np.dot(w,preferedDirectionVector)
+            self.direction -= turnSpeed * tempVector / np.sqrt( np.dot(tempVector, tempVector) )
+        else:
+            self.x += velocity * deltaTime * np.cos(self.direction)
+            self.y += velocity * deltaTime * np.sin(self.direction)
 
 
     def draw(self):
@@ -114,6 +100,18 @@ class Kilobot:
         self.renderer.drawCircle(colorBody, position, size)
         self.renderer.drawLine(colorDirectionLine, position, directionLineTarget, size/5)
         self.renderer.drawText((255, 0, 0), str(self.gradientVal), position)
+
+    def _getBinaryCoord(self):
+        bitMapVal = 0
+        if self.x >= 0 and  self.y >= 0:
+            bitMapX = int(self.x/self.bitMapScalingFactor)
+            bitMapY = int(self.y/self.bitMapScalingFactor)
+
+            bitShape = self.bitMapArray.shape
+            if bitMapX >= 0 and bitMapX < bitShape[0] and bitMapY >= 0 and bitMapY < bitShape[1]:
+                print(bitMapX, bitMapY)
+                bitMapVal = self.bitMapArray[bitMapX, bitMapY]
+        return bitMapVal
 
 
 class KilobotOrigin(Kilobot):
@@ -141,8 +139,3 @@ def getBitMap(file='data/bitmap.csv'):
 
     bitArray =  np.asarray(bitArray)
     bitArray =  np.asmatrix(bitArray)
-
-
-
-
-a = getBitMap()
